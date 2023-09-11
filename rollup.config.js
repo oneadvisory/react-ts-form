a166773de6f3403c035ec89521eeb27931259ddc
Kevin Decker <kpdecker@gmail.com>
2023-09-11 18:13:55 -0500

    Remove terser from build
    Clients can do this for themselves and have better debugging in the library.

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import terser from "@rollup/plugin-terser";
 export default [
   {
     input: "src/index.ts",
-    plugins: [typescript({ tsconfig: "tsconfig.json" }), terser()],
+    plugins: [typescript({ tsconfig: "tsconfig.json" })],
     output: [
       { file: "lib/index.cjs", format: "cjs" },
       { file: "lib/index.mjs", format: "es" },
